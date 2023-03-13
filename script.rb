6ac627fc8a70bcad3a6ab055dac7187ebe4894a0
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-13 13:12:02 +0700

    Split string into array

--- script.rb
+++ script.rb
@@ -2,3 +2,10 @@ dictionary = ["crummy", "crumb", "crumble", "rum", "my", "also", "so", "son", "o
   "commercial", "commerce", "me", "of", "off", "offer", "bit", "itch", "bitch", "it"]
 
 string = "Ovaltine? A crummy commercial? Son of a bitch!"
+
+def substrings(string, dictionary)
+  p array = string.split(" ") 
+  
+end
+
+substrings(string, dictionary)
