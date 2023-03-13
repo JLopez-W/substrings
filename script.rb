757ffa7af1c5de7f6946c25372af107844b256d7
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-13 13:11:04 +0700

    Add dictionary and string

---
+++ script.rb
@@ -0,0 +1,4 @@
+dictionary = ["crummy", "crumb", "crumble", "rum", "my", "also", "so", "son", "oval", "Ovaltine", "malt", "a", 
+  "commercial", "commerce", "me", "of", "off", "offer", "bit", "itch", "bitch", "it"]
+
+string = "Ovaltine? A crummy commercial? Son of a bitch!"
